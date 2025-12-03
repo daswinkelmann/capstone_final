76e82b0dd227886876e166a2807b08425ae4c047
daswinkelmann <daswinkelmann@gmail.com>
2025-12-03 17:19:02 +0000

    created basic form in views and template, and run a get request

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.TaskList.as_view(), name='home'),
+    path('add/', views.TaskCreate.as_view(), name='add_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Task
-
+from .forms import TaskForm
 
 # Create your views here.
 class TaskList(generic.ListView):
@@ -10,3 +10,8 @@ class TaskList(generic.ListView):
     template_name = "tasks/index.html"
     context_object_name = "task_list"
     paginate_by = 6
+
+class TaskCreate(generic.View):
+    def get(self, request):
+        form = TaskForm()
+        return render(request, "tasks/add_task.html", {"form": form})    
