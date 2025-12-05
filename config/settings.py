ea041651ab5cb467181710a248fdeacedad82bac
daswinkelmann <daswinkelmann@gmail.com>
2025-12-05 16:56:05 +0000

    updated DB settings for heroku

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
