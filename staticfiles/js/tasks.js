c7a670377f304f75c12b38dde89a6ecf55c81686
daswinkelmann <daswinkelmann@gmail.com>
2025-12-12 12:43:15 +0000

    added disabled style to task status and DONE button

--- staticfiles/js/tasks.js
+++ staticfiles/js/tasks.js
@@ -1,15 +1,7 @@
-// const deleteModalElement = document.getElementById("deleteModal");
-// const deleteModal = new bootstrap.Modal(deleteModalElement);
-
-// TEMP
-console.log("tasks.js loaded");
-
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
-// TEMP
-console.log("found delete buttons:", deleteButtons.length);
 
 /**
  * Initializes deletion functionality for the provided delete buttons.
@@ -18,8 +10,6 @@ console.log("found delete buttons:", deleteButtons.length);
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
         const taskId = e.currentTarget.getAttribute("data-task-id");
-        // TEMP
-        console.log("clicked delete for task:", taskId);
         deleteConfirm.href = `${taskId}/delete`;
         deleteModal.show();
     });
